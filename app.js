d5848ce5f2db4750ede5d86593d9f9ea8d3e2762
gon_14 <poniemangon@gmail.com>
2023-01-18 20:39:08 -0300

    un par de fixes

--- app.js
+++ app.js
@@ -18,3 +18,7 @@ app.get("/login", (req, res) => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home-page.html"));
 });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
