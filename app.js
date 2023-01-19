3b99e2ec365787a6753cca3b65c2925fc9792f8b
gon_14 <poniemangon@gmail.com>
2023-01-19 12:54:07 -0300

    todo funcionando

--- app.js
+++ app.js
@@ -8,4 +8,15 @@ app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/register.html"));
 });
 
-app.listen(3041, () => console.log("Servidor 3041 corriendo"));
+app.listen(3000, () => console.log("Servidor 3000 corriendo"));
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
+app.get("/product", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/detalle-producto.html"));
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home-page.html"));
+});
