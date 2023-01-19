988da71b3836cf673d4ce265c44a9575dd96b3c8
gon_14 <poniemangon@gmail.com>
2023-01-19 12:48:20 -0300

    home responsive

--- app.js
+++ app.js
@@ -22,3 +22,7 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/register.html"));
 });
+
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/carrito.html"));
+});
