461482b279a372c56b26b2797f14150cd84d7b1f
PabloDiaz84 <119020662+PabloDiaz84@users.noreply.github.com>
2023-01-18 21:00:51 -0300

    Formulario de Registro
    Envío formulario de registro modificado

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 const express = require("express");
-const app = express();
 const path = require("path");
-const PORT = 3000;
+const app = express();
 
-app.use(express.static(path.join(__dirname, "./public")));
-
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
-
-app.get("/detalle-producto", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/detalle-producto.html"));
-});
-
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/login.html"));
-});
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home-page.html"));
-});
+app.use(express.static("public"));
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/register.html"));
-});
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+})
+
+app.listen(3041, () => console.log("Servidor 3041 corriendo"));
