2ba4f7322850158b461927a0f40abb9d931b4d41
j-belec <jeronimobelec@hotmail.com>
2023-03-19 17:24:45 -0300

    inicio sequelize y conexion a bd

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "bd_proyecto_int_dh",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "bd_proyecto_int_dh",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "bd_proyecto_int_dh",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
