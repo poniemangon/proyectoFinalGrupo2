b71d5af41471d6c9adb1b7a8223f91db3e2546e0
gon_14 <poniemangon@gmail.com>
2023-04-13 13:41:51 -0300

    validacion de back para registro y producto

---
+++ src/middlewares/productAddValidation.js
@@ -0,0 +1,62 @@
+const { body } = require('express-validator');
+const path = require('path');
+const sizeOf = require('image-size');
+const fs = require('fs');
+
+ const productAddValidation = [
+    body('name').isLength({min:1, max:50}).withMessage('Nombre no puede estar vacio o es muy largo'),
+    body('description').isLength({min:1, max:255}).withMessage('La descripcion no puede estar vacia o es muy larga (mas de 255 caracteres)'),
+    body('product_image').custom(
+        (value, {req}) =>
+        {   
+            
+            if(req.files.product_image){
+                const file = req.files.product_image[0];
+                const readFile = fs.readFileSync(file.path);
+                const fileExt = path.extname(file.originalname);
+                const extensionesValidas = ['.jpeg', '.png', '.gif', '.jpg'];
+    
+                if(!extensionesValidas.includes(fileExt)){
+                    throw new Error('Solo es valido JPG, PNG y GIF');
+                }
+                if (!(sizeOf(readFile).height / sizeOf(readFile).width == 1.0)){
+                    throw new Error('La foto de producto debe ser cuadrada');
+                }
+                
+                return true;
+            }
+            else {
+                throw new Error('Campo de imagen vacio');
+            }
+
+        },
+    
+    ),
+    body('product_banner').custom(
+        (value, {req}) =>
+        {   
+            if(req.files.product_banner){
+                const file = req.files.product_banner[0];
+                const fileExt = path.extname(file.originalname);
+                const extensionesValidas = ['.jpeg', '.png', '.gif', '.jpg'];
+    
+                if(!extensionesValidas.includes(fileExt)){
+                    throw new Error('Solo es valido JPG, PNG y GIF');
+                }
+
+                
+                return true;
+            }
+            else {
+                throw new Error('Campo de banner vacio');
+            }
+
+        },
+    
+    )
+
+
+];
+
+
+module.exports = productAddValidation;
--- src/middlewares/registerValidation.js
+++ src/middlewares/registerValidation.js
@@ -1,7 +1,9 @@
 const { body } = require('express-validator');
 const path = require('path');
+const sizeOf = require('image-size');
+const fs = require('fs');
 
- const validation = [
+ const registerValidation = [
     body('username').notEmpty().withMessage('Usuario no puede estar vacio'),
     body('email').isEmail().withMessage('Ingresar email valido'),
     body('password').isLength({min: 6}).withMessage('La contrasena debe tener 6 caracteres como minimo'),
@@ -14,11 +16,15 @@ const path = require('path');
             }
             else {
                 const file = req.file;
+                const readFile = fs.readFileSync(file.path);
                 const fileExt = path.extname(file.originalname);
-                const extensionesValidas = ['jpeg', 'png', 'gif', '.jpg'];
+                const extensionesValidas = ['.jpeg', '.png', '.gif', '.jpg'];
 
                 if(!extensionesValidas.includes(fileExt)){
                     throw new Error('Solo es valido JPG, PNG y GIF');
+                }
+                if (!(sizeOf(readFile).height / sizeOf(readFile).width == 1.0)){
+                    throw new Error('La foto de producto debe ser cuadrada');
                 }
             }
             return true;
@@ -30,4 +36,4 @@ const path = require('path');
 ];
 
 
-module.exports = validation;
+module.exports = registerValidation;
