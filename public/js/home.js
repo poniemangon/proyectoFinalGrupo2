a06b573a153980d240df887eb490ba861e2f99b6
j-belec <jeronimobelec@hotmail.com>
2023-05-02 15:40:51 -0300

    Nuevo diseño Home en proceso

---
+++ public/js/home.js
@@ -0,0 +1,13 @@
+let carrousel = document.querySelector(".carrousel-container-inner");
+let imagesArr = document.querySelectorAll(".carrousel-img");
+
+let currentImg = 0;
+setInterval(function () {
+  let transition = currentImg * -100;
+  carrousel.style.transform = `translateX(${transition}%)`;
+  currentImg++;
+
+  if (currentImg > imagesArr.length - 1) {
+    currentImg = 0;
+  }
+}, 4000);
