fb97bea12b2856ced87764073b4ee7c44f5e328e
j-belec <jeronimobelec@hotmail.com>
2023-05-04 12:49:18 -0300

    Creo productsCasuales view y productsDeportivas view

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,165 +1,204 @@
 const fs = require("fs");
-const path = require("path"); 
-const db = require('../database/models/');
-const multer = require('multer');
-const upload = multer({ dest: 'public/images/products' });
-const { validationResult } = require('express-validator');
-
-
+const path = require("path");
+const db = require("../database/models/");
+const multer = require("multer");
+const upload = multer({ dest: "public/images/products" });
+const { validationResult } = require("express-validator");
 
 const productsController = {
-	carrito: (req, res) => {
-		const productos = req.session.carrito;
-		return res.render('carrito', {productos});
-	},
-	detail: async (req, res) => {
-		const id = req.params.id;
-		try {
-			const product = await db.Product.findByPk(id);
-			return res.render('detalle-producto', {product});
-		}
-		catch (err) {
-			return res.redirect('back');
-		}
-	},
-	addToCart: async (req, res) => {
-		if (req.session.user){
-			console.log('hay user logueado');
+  carrito: (req, res) => {
+    const productos = req.session.carrito;
+    return res.render("carrito", { productos });
+  },
 
-			
-		}
+  detail: async (req, res) => {
+    const id = req.params.id;
+    try {
+      const product = await db.Product.findByPk(id);
+      return res.render("detalle-producto", { product });
+    } catch (err) {
+      return res.redirect("back");
+    }
+  },
 
-		
-		const id = req.body.productId;
-		const product = await db.Product.findByPk(id);
-		product.dataValues.numeroCarrito = (req.session.carrito.length + 1);
-		req.session.carrito.push(product.dataValues);
-		console.log(req.session.carrito);
-		return res.redirect('back');
-		
-	},
-	create: async (req, res) => {
-		const categorias = await db.ProductCategory.findAll();
-		
-		return res.render('agregar', {categorias});
-	},
-	store: async (req, res) => {
-		console.log(req.body, req.file);
-		const errors = validationResult(req).array();
-		const categorias = await db.ProductCategory.findAll();
-		if (!errors.length == 0){
-			
-				if (req.file) {fs.unlinkSync(req.file.path)};
-			  
-			  console.log(errors);
-			return res.render('agregar', {categorias, errors, oldData: req.body});
-		}
-		else {
-			try {
-				// await validateNewProduct(req, res, () => {});
-				const product = await db.Product.create({
-					name: req.body.name,
-					description: req.body.description,
-					price: req.body.price,
-					image: req.file.filename,
-					banner: 'BANNER.png',
-					id_product_category: req.body.id_product_category
-				});
-				return res.redirect('/');
-			}
-			catch (error) {
-				console.error(error);
-				return res.redirect('back');
-			  }
-	
-		}
-		
+  addToCart: async (req, res) => {
+    if (req.session.user) {
+      console.log("hay user logueado");
+    }
+    const id = req.body.productId;
+    const product = await db.Product.findByPk(id);
+    product.dataValues.numeroCarrito = req.session.carrito.length + 1;
+    req.session.carrito.push(product.dataValues);
+    console.log(req.session.carrito);
+    return res.redirect("back");
+  },
 
-	},
-	edit: async (req, res) => {
-		const categorias = await db.ProductCategory.findAll();
-		const id = await req.params.id;
-		const product = await db.Product.findOne({ where: { id } });
-		if (product) {
-		return res.render('editar-producto', {product, categorias});
-		}
-		else {
-		  return res.redirect('/');
-		}
-	},
-	update: async (req, res) => {
-		console.log(req.body, req.file);
-		const errors = validationResult(req).array();
-		const categorias = await db.ProductCategory.findAll();
-		if (!errors.length == 0){
-				console.log(errors);
-				if (req.file) {fs.unlinkSync(req.file.path)};
-			  
-			  console.log(errors);
-			return res.render('agregar', {categorias, errors, oldData: req.body});
-		}
-		const id = req.params.id;
-		const product = await db.Product.findOne({ where: { id } });
-		const imagePath = path.join(__dirname, '..', '..', 'public', 'images', 'products', product.image);
-		if (!errors.length == 0){
-			return res.render('editar-producto', {categorias, oldData: req.body, errors} );
-		}
-		if (req.file) {
-		  fs.unlink(imagePath, (err) => {
-			if (err) {
-			  console.error(err);
-			}
-		  });
-		}
-		
-	  
-		const newData = {
-		  name: req.body.name,
-		  description: req.body.description,
-		  price: req.body.price,
-		  image: req.file ? req.file.filename : product.image,
-		  banner: 'BANNER.PNG',
-		  id_product_category: req.body.id_product_category
-		};
-		console.log(newData.image);
-		await db.Product.update(newData, { where: { id } });
-		return res.redirect(`/products/detail/${id}`);
-	  },
-	  delete: async (req, res) => {
-		const id = req.params.id;
-		try {
-		  // Find the product to delete
-		  const product = await db.Product.findByPk(id);
-		  if (!product) {
-			return res.status(404).send('Product not found');
-		  }
-		  console.log(product);
-		  // Delete the product image and banner files
-		  const imagePath = path.join(__dirname, '..', '..', 'public', 'images', 'products', product.image);
-		  const bannerPath = path.join(__dirname, '..', '..', 'public', 'images', 'products', product.banner);
-		  fs.unlink(imagePath, (err) => {
-			if (err) {
-			  console.error(err);
-			}
-		  });
-		  fs.unlink(bannerPath, (err) => {
-			if (err) {
-			  console.error(err);
-			}
-		  });
-	
-		  // Delete the product from the database
-		  await product.destroy();
-		  return res.redirect('/');
-		} catch (error) {
-		  console.error(error);
-		  return res.redirect('back');
-		}
-	  }
-	};
+  create: async (req, res) => {
+    const categorias = await db.ProductCategory.findAll();
 
-	  
-	
+    return res.render("agregar", { categorias });
+  },
 
+  store: async (req, res) => {
+    console.log(req.body, req.file);
+    const errors = validationResult(req).array();
+    const categorias = await db.ProductCategory.findAll();
+    if (!errors.length == 0) {
+      if (req.file) {
+        fs.unlinkSync(req.file.path);
+      }
+
+      console.log(errors);
+      return res.render("agregar", { categorias, errors, oldData: req.body });
+    } else {
+      try {
+        // await validateNewProduct(req, res, () => {});
+        const product = await db.Product.create({
+          name: req.body.name,
+          description: req.body.description,
+          price: req.body.price,
+          image: req.file.filename,
+          banner: "BANNER.png",
+          id_product_category: req.body.id_product_category,
+        });
+        return res.redirect("/");
+      } catch (error) {
+        console.error(error);
+        return res.redirect("back");
+      }
+    }
+  },
+
+  edit: async (req, res) => {
+    const categorias = await db.ProductCategory.findAll();
+    const id = await req.params.id;
+    const product = await db.Product.findOne({ where: { id } });
+    if (product) {
+      return res.render("editar-producto", { product, categorias });
+    } else {
+      return res.redirect("/");
+    }
+  },
+
+  update: async (req, res) => {
+    console.log(req.body, req.file);
+    const errors = validationResult(req).array();
+    const categorias = await db.ProductCategory.findAll();
+    if (!errors.length == 0) {
+      console.log(errors);
+      if (req.file) {
+        fs.unlinkSync(req.file.path);
+      }
+
+      console.log(errors);
+      return res.render("agregar", { categorias, errors, oldData: req.body });
+    }
+    const id = req.params.id;
+    const product = await db.Product.findOne({ where: { id } });
+    const imagePath = path.join(
+      __dirname,
+      "..",
+      "..",
+      "public",
+      "images",
+      "products",
+      product.image
+    );
+    if (!errors.length == 0) {
+      return res.render("editar-producto", {
+        categorias,
+        oldData: req.body,
+        errors,
+      });
+    }
+    if (req.file) {
+      fs.unlink(imagePath, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
+
+    const newData = {
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      image: req.file ? req.file.filename : product.image,
+      banner: "BANNER.PNG",
+      id_product_category: req.body.id_product_category,
+    };
+    console.log(newData.image);
+    await db.Product.update(newData, { where: { id } });
+    return res.redirect(`/products/detail/${id}`);
+  },
+
+  delete: async (req, res) => {
+    const id = req.params.id;
+    try {
+      // Find the product to delete
+      const product = await db.Product.findByPk(id);
+      if (!product) {
+        return res.status(404).send("Product not found");
+      }
+      console.log(product);
+      // Delete the product image and banner files
+      const imagePath = path.join(
+        __dirname,
+        "..",
+        "..",
+        "public",
+        "images",
+        "products",
+        product.image
+      );
+      const bannerPath = path.join(
+        __dirname,
+        "..",
+        "..",
+        "public",
+        "images",
+        "products",
+        product.banner
+      );
+      fs.unlink(imagePath, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+      fs.unlink(bannerPath, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+
+      // Delete the product from the database
+      await product.destroy();
+      return res.redirect("/");
+    } catch (error) {
+      console.error(error);
+      return res.redirect("back");
+    }
+  },
+
+  casualProducts: async (req, res) => {
+    const allProducts = await db.Product.findAll();
+    const casualProducts = allProducts.filter(
+      (product) => product.id_product_category == 1
+    );
+
+    return res.render("productsCasuales", { casualProducts });
+  },
+
+  sportingProducts: async (req, res) => {
+    const allProducts = await db.Product.findAll();
+    const sportingProducts = allProducts.filter(
+      (product) => product.id_product_category == 2
+    );
+
+    console.log(sportingProducts);
+
+    return res.render("productsDeportivas", { sportingProducts });
+  },
+};
 
 module.exports = productsController;
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,46 +1,57 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer')
+const multer = require("multer");
 const productsController = require("../controllers/productsController");
-const path = require('path');
+const path = require("path");
 const productAddValidation = require("../middlewares/productAddValidation");
 const productEditValidation = require("../middlewares/productEditValidation");
 
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "../../public/images/products"));
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    }
-  })
-  
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../../public/images/products"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
 
+const upload = multer({ storage: storage });
 
 //Todos los productos
 
 router.get("/", productsController.carrito);
-
+router.get("/casuales", productsController.casualProducts);
+router.get("/deportivas", productsController.sportingProducts);
 
 //Crear un producto
 router.get("/create", productsController.create);
-router.post('/store', upload.single('product_image'), productAddValidation, productsController.store); 
+router.post(
+  "/store",
+  upload.single("product_image"),
+  productAddValidation,
+  productsController.store
+);
 
 //Detalle producto
 router.get("/detail/:id", productsController.detail);
 router.post("/add", productsController.addToCart);
 
 //Editar un producto
-router.get('/edit/:id', productsController.edit); 
-router.put('/edit/:id', upload.single('product_image'), productEditValidation, productsController.update); 
+router.get("/edit/:id", productsController.edit);
+router.put(
+  "/edit/:id",
+  upload.single("product_image"),
+  productEditValidation,
+  productsController.update
+);
 // carrito
-router.get('/carrito', productsController.carrito);
-router.get('/delete/:id', productsController.delete);
-router.delete('/delete/:id', productsController.delete);
-
+router.get("/carrito", productsController.carrito);
+router.get("/delete/:id", productsController.delete);
+router.delete("/delete/:id", productsController.delete);
 
 // delete
 // router.delete('/delete/:id', productsController.destroy);
