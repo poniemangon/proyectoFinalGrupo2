ba78462f61491e545a6d47595c227aa5e272fa8f
gon_14 <poniemangon@gmail.com>
2023-05-08 13:40:39 -0300

    dinamizando categorias

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -10,7 +10,7 @@ const mainController = {
     home: async (req,res) => {
         const products = await db.Product.findAll();
         const bannerProduct = await db.Product.findByPk(12);
-        const products1 = await products.slice(products.length-7, products.length).reverse();
+        const products1 = await products.slice(products.length-8, products.length).reverse();
 
         
         console.log(bannerProduct.dataValues);
