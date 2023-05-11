e2edfa02ee16c288767113496ae89b4ac8bf3243
j-belec <jeronimobelec@hotmail.com>
2023-05-11 12:51:04 -0300

    Corrigiendo y arreglando detalles en profile y changeUserForm

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,30 +1,26 @@
-const path = require('path');
-const fs = require('fs');
-const productsFilePath = path.join(__dirname, '../data/products.json');
-const db = require('../database/models/');
-
-
-
+const path = require("path");
+const fs = require("fs");
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const db = require("../database/models/");
 
 const mainController = {
-    home: async (req,res) => {
-        if(!req.session.carrito){
-            req.session.carrito = [];
-          }
-        console.log(req.session.carrito);
-        try{
-        const products = await db.Product.findAll();
+  home: async (req, res) => {
+    if (!req.session.carrito) {
+      req.session.carrito = [];
+    }
+    console.log(req.session.carrito);
+    try {
+      const products = await db.Product.findAll();
 
-        const products1 = await products.slice(products.length-8, products.length).reverse();
+      const products1 = await products
+        .slice(products.length - 8, products.length)
+        .reverse();
 
-        
-
-        return res.render("home-page", {products1} );
-        }
-        catch (error){
-            return res.render('404', { error: 'Error.' });
-          }
-    },
+      return res.render("home-page", { products1 });
+    } catch (error) {
+      return res.render("404", { error: "Error." });
+    }
+  },
 };
 
 module.exports = mainController;
